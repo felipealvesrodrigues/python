6dad70c5602c5f1de2fe978187088bffadf515c7
Felipe Alves Rodigues <alvesrodriguesfelipe8@gmail.com>
2025-12-19 10:32:02 -0300

    manipulando strings

--- cursoemvideo/aula09a.py
+++ cursoemvideo/aula09a.py
@@ -7,5 +7,10 @@ print(frase[3:13]) """
 """ frase = 'Curso em Vídeo Python'
 print(frase[1:15:2]) """
 
+""" frase = 'Curso em Vídeo Python'
+frase = frase.upper()
+print(frase) """
+
 frase = 'Curso em Vídeo Python'
-print(len(frase))
+dividido = frase.split()
+print(dividido[0])
